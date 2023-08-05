8203c7122516ed2b3c832ff4afd9b93d83174238
Makoto Satake <satake.syukatsu@gmail.com>
2023-08-05 19:40:55 +0900

    比較演算子を実装。

--- test.sh
+++ test.sh
@@ -18,6 +18,7 @@ assert() {
 
 assert 0 0
 assert 42 42
+assert 2 "1+1"
 assert 21 "5+20-4"
 assert 41 " 12 + 34 - 5 "
 assert 47 '5+6*7'
@@ -25,5 +26,19 @@ assert 15 '5*(9-6)'
 assert 4 '(3+5)/2'
 assert 10 '-10+20'
 assert 5 '+2+3'
+assert 1 "22 == 22"
+assert 0 "1==2"
+assert 1 "12 != 3"
+assert 0 "3 != 3"
+assert 1 "12 > 10"
+assert 0 "10 > 10"
+assert 1 "30 >= 3"
+assert 1 "2 >= 2"
+assert 0 "13 >= 15"
+assert 1 "12 < 16"
+assert 0 "5 < 2"
+assert 1 "12 <= 23"
+assert 1 "5 <= 5"
+assert 0 "9 <= 4"
 
 echo OK
